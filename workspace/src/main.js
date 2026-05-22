47d6e67b297758f0317f3f821524661e2f17bc2a
xDemoxz <nicolasguarinr@gmail.com>
2026-05-22 15:16:50 -0500

    SS

--- workspace/src/main.js
+++ workspace/src/main.js
@@ -3,6 +3,33 @@ import './style.css'
 async function getData() {
   const respense = await fetch("http://localhost:3000/tasks")
   const data = await respense.json()
+
+  return data
 }
 
+const tasks = await getData() 
+const addBtn = document.getElementById("addBtn")
+const listTask = document.getElementById("listTask")
+
+const taskNew = document.createElement('li')
+
+const title = tasks.map(task  => { // this variable 
+  return task.title
+})
+
+taskNew.textContent = title
+listTask.appendChild(taskNew)
+
+
+addBtn.addEventListener("click", () => {
+  const taskNew = document.createElement('li')
+
+  const title = tasks.map(task  => { // this variable 
+  return task.title
+})
+
+
+  taskNew.textContent = title
+  listTask.appendChild(taskNew)
+})
 
